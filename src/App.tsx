f9ee8ecb104b3eb019e4573b0ac1c2b163619b64
Sergei Treigo <s.treigo@techin.by>
2023-12-28 13:20:10 +0300

    feat lazy page Cart and DetailPizza

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import PizzaDetail from './pages/PizzaDetail';
 import MainLayout from './layouts/MainLayout';
+
+const Cart = React.lazy(() => import('./pages/Cart'));
+const PizzaDetail = React.lazy(() => import('./pages/PizzaDetail'));
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<MainLayout />}>
-                <Route path="" element={<Home />} />
-                <Route path="cart" element={<Cart />} />
-                <Route path="pizzas/:id" element={<PizzaDetail />} />
-                <Route path="*" element={<NotFound />} />
-            </Route>
-        </Routes>
+        <Suspense fallback={<div>Загрузка страницы</div>}>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route path="" element={<Home />} />
+                    <Route path="cart" element={<Cart />} />
+                    <Route path="pizzas/:id" element={<PizzaDetail />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+        </Suspense>
     );
 }
 
